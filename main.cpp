612759ad28485c16491cd45c973e1c1c60e0e804
Stefano Savarino (external) <stefano.savarino@husqvarnagroup.com>
2023-05-24 15:17:50 +0200

    day2 part 1 added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -8,6 +9,7 @@ using namespace std;
 
 // Functions declaration
 void day1();
+void day2();
 
 // Classes
 class Elf {
@@ -49,7 +51,8 @@ class Elf {
 int main() {
     cout << "AoC 2023" << endl;
 
-    day1();
+    // day1();
+    day2();
 
    return 0;
 }
@@ -101,4 +104,79 @@ void day1() {
 
     cout << "Sum of top3 elfs calories: " << total_top3 << " calories." << endl;
 
+}
+
+void day2() {
+    cout << "### Day 2 ###" << endl;
+    
+    const string filename = "C:/Users/H780521/Documents/AoC/Inputs/Day2.txt";
+    ifstream input_file(filename);
+    string line, player1, player2;
+
+    int total_score = 0;
+    
+    while(getline(input_file, line, '\n')) {
+        stringstream round(line);
+        getline(round, player1, ' ');
+        getline(round, player2, ' ');
+        
+        switch(player1[0]) {
+            case 'A':
+                if(player2[0] == 'X') {
+                    // player2 played Rock, 1 point
+                    total_score++;
+                    // result is a draw, 3 points
+                    total_score += 3;
+                } else if(player2[0] == 'Y') {
+                    // player2 played Paper, 2 points
+                    total_score += 2;
+                    // result is a win, 6 points
+                    total_score += 6;
+                } else if(player2[0] == 'Z') {
+                    // player2 played Scissors, 3 points
+                    total_score += 3;
+                    // result is a loss, 0 points
+                }
+                break;
+            case 'B':
+                if(player2[0] == 'X') {
+                    // player2 played Rock, 1 point
+                    total_score++;
+                    // result is a loss, 0 points
+                } else if(player2[0] == 'Y') {
+                    // player2 played Paper, 2 points
+                    total_score += 2;
+                    // result is a draw, 3 points
+                    total_score += 3;
+                } else if(player2[0] == 'Z') {
+                    // player2 played Scissors, 3 points
+                    total_score += 3;
+                    // result is a win, 6 points
+                    total_score += 6;
+                }
+                break;
+            case 'C':
+                if(player2[0] == 'X') {
+                    // player2 played Rock, 1 point
+                    total_score++;
+                    // result is a win, 6 points
+                    total_score += 6;
+                } else if(player2[0] == 'Y') {
+                    // player2 played Paper, 2 points
+                    total_score += 2;
+                    // result is a loss, 0 points
+                } else if(player2[0] == 'Z') {
+                    // player2 played Scissors, 3 points
+                    total_score += 3;
+                    // result is a draw, 3 points
+                    total_score += 3;
+                }
+                break;
+            default:
+                cout << "ERROR: invalid input" << endl;
+                break;
+        } 
+    }
+
+    cout << "Total score: " << total_score << " points." << endl;
 }
